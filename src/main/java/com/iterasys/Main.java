b79a0dea4306d7817c5247a2a3089fac012ac344
erica-qa <erica.santos.qa@gmail.com>
2024-10-15 21:26:52 -0300

    first commit

---
+++ src/main/java/com/iterasys/Main.java
@@ -0,0 +1,57 @@
+// 1 - Bibliotecas / Imports
+
+// 1.1 - Pacotes
+
+package com.iterasys; // pacote é um coletivo de classes
+
+// 2 - Classe
+
+public class Main {
+
+    // 2.1 Atributos
+
+    // 2.2 Métodos e Funções
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+       // float resultado = somar(num1:5,num2:7);
+       // System.out.println("A soma de 5 + 7 = " + resultado);
+    }
+
+    public static float somar(float num1, float num2) {
+        float resultado = num1 + num2;
+        System.out.println("Soma de " + num1 + " + " + num2 + " = " + resultado);
+        return resultado;
+    }
+
+    public static float subtrair(float num1, float num2) {
+        float resultado = num1 - num2;
+        System.out.println("Subtração de " + num1 + " - " + num2 + " = " + resultado);
+        return resultado;
+    }
+
+    public static float multiplicar(float num1, float num2) {
+        float resultado = num1 * num2;
+        System.out.println("Multiplicação de " + num1 + " * " + num2 + " = " + resultado);
+        return resultado;
+    }
+
+    public static float dividirIf(float num1, float num2) {
+        if (num2 != 0) {
+            return num1 / num2;    
+        }
+        else{
+            System.out.println("Não é possível dividir por zero");
+            return 0;
+        }
+    }
+
+    public static String dividirTry(int num1, int num2) {
+        try{
+            return String.valueOf(num1 / num2);
+        } catch (Exception e){
+            return "Não é possível dividir por zero";
+        }
+    }
+
+}
